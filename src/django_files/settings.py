8661131d110d5a1df92f14675ab55f92725c0a60
isaac868 <33269071+isaac868@users.noreply.github.com>
2024-10-19 15:45:44 -0700

    Removed generated files and generalized db connection

--- src/django_files/settings.py
+++ src/django_files/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from os import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'django_files.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': environ.get('DJANGO_DB_NAME'),
+        'USER': environ.get('DJANGO_DB_USER'),
+        'PASSWORD': environ.get('DJANGO_DB_PASS'),
+        'HOST': environ.get('DJANGO_DB_HOST'),
+        'PORT': environ.get('DJANGO_DB_PORT')
     }
 }
 
