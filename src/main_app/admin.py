7b2330585cd5c887114f1517abbfddee57b36bec
isaac868 <33269071+isaac868@users.noreply.github.com>
2024-12-01 19:13:56 -0800

    Added bank and account to admin

--- src/main_app/admin.py
+++ src/main_app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import ParseRule, Category, CategoryRule, Transaction
+from .models import ParseRule, Category, CategoryRule, Transaction, Bank, Account
 
 admin.site.register(ParseRule)
 admin.site.register(Category)
 admin.site.register(CategoryRule)
 admin.site.register(Transaction)
+admin.site.register(Bank)
+admin.site.register(Account)
