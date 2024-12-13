bcd71f0d172b20aaa603c9fbadd10af536eb11b0
isaac868 <33269071+isaac868@users.noreply.github.com>
2024-12-12 23:25:45 -0800

    Added user column to admin panel model listings

--- src/main_app/admin.py
+++ src/main_app/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from .models import ParseRule, Category, CategoryRule, Transaction, Bank, Account
 
-admin.site.register(ParseRule)
-admin.site.register(Category)
-admin.site.register(CategoryRule)
-admin.site.register(Transaction)
-admin.site.register(Bank)
-admin.site.register(Account)
+class DisplayUserAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "user"]
+
+
+class CategoryRuleAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "category__user"]
+
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "bank__user"]
+
+admin.site.register(ParseRule, DisplayUserAdmin)
+admin.site.register(Category, DisplayUserAdmin)
+admin.site.register(CategoryRule, CategoryRuleAdmin)
+admin.site.register(Transaction, DisplayUserAdmin)
+admin.site.register(Bank, DisplayUserAdmin)
+admin.site.register(Account, AccountAdmin)
